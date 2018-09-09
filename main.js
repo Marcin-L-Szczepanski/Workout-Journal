7155795080b075b01ec956a17be10323fe5c5ca4
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-09-09 21:21:44 +0900

    Add cacheDOM and bindEvents functions

--- main.js
+++ main.js
@@ -3,6 +3,19 @@ const WorkoutJournal = (function() {
     console.log("initialize");
   };
 
+  // Shorthand functions
+  const get = element => document.getElementById(element);
+
+  let DOM = {};
+
+  const cacheDOM = () => {
+    DOM.createNewPlanBtn = get("create-plan-btn");
+  };
+
+  const bindEvents = () => {
+    click(DOM.createNewPlanBtn, openNewPlanPage);
+  };
+
   return {
     init: init
   };
