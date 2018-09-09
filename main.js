da741d5ac12f072223ac7ab3b2675998300922d1
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-09-09 21:35:00 +0900

    Open new plan page

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 const WorkoutJournal = (function() {
   const init = () => {
-    console.log("initialize");
+    cacheDOM();
+    bindEvents();
   };
 
   // Shorthand functions
   const get = element => document.getElementById(element);
+  const newElement = element => document.createElement(element);
+  const click = (element, action) => element.addEventListener("click", action);
+  const showPage = page => page.classList.remove("page--hidden");
+  const hidePage = page => page.classList.add("page--hidden");
 
   let DOM = {};
 
   const cacheDOM = () => {
     DOM.createNewPlanBtn = get("create-plan-btn");
+    DOM.newPlanPage = get("new-plan-page");
   };
 
   const bindEvents = () => {
     click(DOM.createNewPlanBtn, openNewPlanPage);
+  };
+
+  const openNewPlanPage = () => {
+    console.log("bla");
+    showPage(DOM.newPlanPage);
   };
 
   return {
