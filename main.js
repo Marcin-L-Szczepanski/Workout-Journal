6347f0880127c66460f9648d335b3ae56e7529e6
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-09-10 06:34:03 +0900

    Add exercise categories module

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import exerciseCategories from "exercise-categories.js";
+
 const WorkoutJournal = (function() {
   const init = () => {
     cacheDOM();
@@ -23,8 +25,39 @@ const WorkoutJournal = (function() {
   };
 
   const openNewPlanPage = () => {
-    console.log("bla");
+    renderNewPlanPage();
     showPage(DOM.newPlanPage);
+  };
+
+  const renderNewPlanPage = () => {
+    const name = newElement("input");
+    const exercisesList = newElement("div");
+    const saveNewPlanBtn = newElement("button");
+    saveNewPlanBtn.innerHTML = "Save Plan";
+
+    exercisesList.classList.add("exercises-list");
+
+    exerciseCategories.forEach(category => {
+      const exerciseDiv = newElement("div");
+      const exerciseIcon = newElement("div");
+
+      exerciseIcon.innerHTML = category.icon;
+      exerciseIcon.classList.add("exercise-icon");
+      exerciseDiv.addEventListener("click", () => {
+        openNewPlanExercisePage(category);
+      });
+
+      exerciseDiv.appendChild(exerciseIcon);
+      exercisesList.appendChild(exerciseDiv);
+    });
+
+    saveNewPlanBtn.addEventListener("click", () => {
+      saveNewPlan(name);
+    });
+
+    DOM.newPlanPage.appendChild(name);
+    DOM.newPlanPage.appendChild(exercisesList);
+    DOM.newPlanPage.appendChild(saveNewPlanBtn);
   };
 
   return {
