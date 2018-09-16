a0f4583886ae90d5c74d299f79506f57923b756b
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-09-16 19:57:20 +0900

    Check local storage and hide or show workout button

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 "use strict";
 
 import { cacheDOM, bindEvents } from "./lib/DOM.js";
+import { savedPlans } from "./lib/local-storage.js";
 
 const WorkoutJournal = (function() {
   const init = () => {
-    cacheDOM();
+    let DOM = cacheDOM();
+    if (savedPlans() !== null) {
+      DOM.startWorkoutBtn.classList.remove("hidden");
+    }
     bindEvents();
   };
 
